0357170d93c3bbcd1751010df5e2929865a6486c
Daniel Sasse <dsasse07@gmail.com>
2020-12-10 14:31:35 -0500

    commit tree 4dbb9f27a7bb1b1af1462e401815787f83373536

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -10,4 +11,55 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(song_name)
+    song = Song.new
+    song.name= song_name
+    song
+  end
+
+  def self.create_by_name(song_name)
+    song = self.new_by_name(song_name)
+    song.save
+    song
+  end
+
+  def self.find_by_name(song_name)
+    self.all.find {|song| song.name == song_name}  
+  end
+
+  def self.find_or_create_by_name(song_name)
+    self.find_by_name(song_name) || self.create_by_name(song_name)
+  end
+
+  def self.alphabetical
+    self.all.sort_by {|song| song.name}
+  end
+
+  def self.new_from_filename(filename_par)
+    song_info = filename_par.chomp(".mp3").split(" - ")
+    song = self.new_by_name(song_info[1])
+    song.artist_name = song_info[0]
+    song
+  end
+
+  def self.create_from_filename(filename_par)
+    song = self.new_from_filename(filename_par)
+    song.save
+  end
+
+  def self.destroy_all
+    self.all.clear    
+  end
 end
+
+song = Song.create_by_name("Savior")
+the_middle = Song.create_by_name("The Middle")
+toxic = Song.create_by_name("Toxic")
+song2 = Song.new_from_filename("Taylor Swift - Blank Space.mp3")
+
